326d517eefa89a47e8068fb3b6ec3d5ab70341a0
Avery Regier <avery.regier@gmail.com>
2014-03-02 22:21:24 -0600

    an initial pom

---
+++ src/main/java/score-source/App.java
@@ -0,0 +1,13 @@
+package score-source;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
