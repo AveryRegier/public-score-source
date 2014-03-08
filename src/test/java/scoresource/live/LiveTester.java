881da7903f6d1b158d56aecd35d212972635d12c
Avery Regier <avery.regier@gmail.com>
2014-03-07 19:47:01 -0600

    refactor

--- src/test/java/scoresource/live/LiveTester.java
+++ src/test/java/scoresource/live/LiveTester.java
@@ -5,7 +5,6 @@ import com.tournamentpool.controller.autoupdate.LiveGame;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,22 +15,25 @@ public class LiveTester {
     public static void main(String... args) throws IOException {
         URI uri = URI.create(args[0]);
         String s = readFully(uri.toURL().openStream());
-        List<LiveGame> games = new XmlRssSource().getGames(s);
-        for(LiveGame game: games) {
-            StringBuffer sb = new StringBuffer();
-            for(Map.Entry<String, Integer> side: game.getPlayerScores().entrySet()) {
-                String team = side.getKey();
-                if(team.equals(game.getWinner())) {
-                    sb.append('*');
-                }
-                sb.append(team);
-                sb.append(" (");
-                sb.append(side.getValue());
-                sb.append(") ");
-            }
-            sb.append(game.getStatus());
-            System.out.println(sb);
+        for(LiveGame game: new XmlRssSource().getGames(s)) {
+            System.out.println(presentResults(game));
         }
+    }
+
+    private static String presentResults(LiveGame game) {
+        StringBuffer sb = new StringBuffer();
+        for(Map.Entry<String, Integer> side: game.getPlayerScores().entrySet()) {
+            String team = side.getKey();
+            if(team.equals(game.getWinner())) {
+                sb.append('*');
+            }
+            sb.append(team);
+            sb.append(" (");
+            sb.append(side.getValue());
+            sb.append(") ");
+        }
+        sb.append(game.getStatus());
+        return sb.toString();
     }
 
     public static String readFully(InputStream in) {
