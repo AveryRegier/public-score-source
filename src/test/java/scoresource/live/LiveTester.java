c13bd31cc79d36fa0b20cfe0f53d1ceace435fab
Avery Regier <avery.regier@gmail.com>
2014-03-07 19:32:35 -0600

    A simple routine to validate integration.

---
+++ src/test/java/scoresource/live/LiveTester.java
@@ -0,0 +1,42 @@
+package scoresource.live;
+
+import com.tournamentpool.controller.autoupdate.LiveGame;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by avery on 3/7/14.
+ */
+public class LiveTester {
+
+    public static void main(String... args) throws IOException {
+        URI uri = URI.create(args[0]);
+        String s = readFully(uri.toURL().openStream());
+        List<LiveGame> games = new XmlRssSource().getGames(s);
+        for(LiveGame game: games) {
+            StringBuffer sb = new StringBuffer();
+            for(Map.Entry<String, Integer> side: game.getPlayerScores().entrySet()) {
+                String team = side.getKey();
+                if(team.equals(game.getWinner())) {
+                    sb.append('*');
+                }
+                sb.append(team);
+                sb.append(" (");
+                sb.append(side.getValue());
+                sb.append(") ");
+            }
+            sb.append(game.getStatus());
+            System.out.println(sb);
+        }
+    }
+
+    public static String readFully(InputStream in) {
+        try (java.util.Scanner s = new java.util.Scanner(in)) {
+            return s.useDelimiter("\\A").hasNext() ? s.next() : "";
+        }
+    }
+}
